27f1d2d86b5f1b45b1f46b6c252b770bcd83db1f
kalpovskii_a <aserenru@gmail.com>
2022-10-06 16:09:53 +0300

    added Card, Drawer and Header components

---
+++ src/components/Drawer.js
@@ -0,0 +1,49 @@
+function Drawer() {
+    return (
+        <div  className="overlay">
+        <div className="drawer">
+        <h2 className="mb-30 d-flex justify-between">Корзина 
+          <img className="removeBtn cu-p" src="/img/btn_cross.svg" alt="remove" />
+        </h2>
+          <div className="items flex">
+          <div className="cart_item d-flex align-center mb-20">
+          <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg">
+          </div>
+          <div className="mr-20 flex">
+            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+            <b>12 999 руб.</b>
+          </div>
+          <img className="removeBtn" src="/img/btn_cross.svg" alt="remove" />
+        </div>
+        <div className="cart_item d-flex align-center mb-20">
+          <div style={{backgroundImage: 'url(/img/sneakers/2.jpg)'}} className="cartItemImg">
+          </div>
+          <div className="mr-20 flex">
+            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+            <b>12 999 руб.</b>
+          </div>
+          <img className="removeBtn" src="/img/btn_cross.svg" alt="remove" />
+        </div>
+          
+          </div>
+          <div className="cartTotalBlock">
+          <ul>
+            <li>
+              <span>Итого: </span>
+              <div></div>
+              <b>21 498 руб. </b>
+            </li>
+            <li>
+              <span>Налог 5%: </span>
+              <div></div>
+              <b>1074 руб. </b>
+            </li>
+          </ul>
+          <button>Оформить заказ</button>
+          </div>
+      </div>
+      </div>
+    );
+}
+
+export default Drawer;
