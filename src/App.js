c0433c63dc553f9b9ed7ffa0961ac1c60cf38792
kalpovskii_a <aserenru@gmail.com>
2022-10-08 11:26:32 +0300

    Added search and delete from cart option

--- src/App.js
+++ src/App.js
@@ -1,51 +1,83 @@
 import Card from "./components/Card";
 import Header from "./components/Header.js";
 import Drawer from "./components/Drawer.js";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 function App() {
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [cartOpened, setCartOpened] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
 
   const onAddToCart = (obj) => {
+    axios
+      .post("https://634014c0e44b83bc73c878f9.mockapi.io/cart", obj);
     setCartItems((prev) => [...prev, obj]);
   };
 
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const onRemoveItem = (id) => {
+    console.log(id);
+    // axios
+    // .delete(`https://634014c0e44b83bc73c878f9.mockapi.io/cart/${id}`);
+  setCartItems((prev) => prev.filter(item => item.id !== id));
+  };
+
   useEffect(() => {
-    fetch("https://634014c0e44b83bc73c878f9.mockapi.io/items")
+    axios
+      .get("https://634014c0e44b83bc73c878f9.mockapi.io/items")
       .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setItems(json);
+        setItems(res.data);
+      });
+
+      axios
+      .get("https://634014c0e44b83bc73c878f9.mockapi.io/cart")
+      .then((res) => {
+        setCartItems(res.data);
       });
   }, []);
 
   return (
     <div className="wrapper clear">
       {cartOpened && (
-        <Drawer items={cartItems} onClose={() => setCartOpened(false)} />
+        <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />
       )}
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center mb-40 justify-between">
-          <h1>Все кроссовки</h1>
+          <h1>
+            {searchValue
+              ? `Поиск по запросу "${searchValue}"`
+              : "Все кроссовки"}
+          </h1>
           <div className="search_block d-flex">
             <img src="/img/scope.svg" alt="search" />
-            <input placeholder="Поиск..." />
+            <input
+              onChange={onChangeSearchValue}
+              value={searchValue}
+              placeholder="Поиск..."
+            />
           </div>
         </div>
         <div className="d-flex flex-wrap">
-          {items.map((item) => (
-            <Card
-              title={item.name}
-              price={item.price}
-              image={item.image}
-              onPlus={onAddToCart}
-              onFavorite={() => console.log("onfav")}
-            />
-          ))}
+          {items
+            .filter((item) =>
+              item.name.toLowerCase().includes(searchValue.toLocaleLowerCase())
+            )
+            .map((item, index) => (
+              <Card
+                key={index}
+                title={item.name}
+                price={item.price}
+                image={item.image}
+                onPlus={onAddToCart}
+                onFavorite={() => console.log("onfav")}
+              />
+            ))}
         </div>
       </div>
     </div>
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,4 +1,4 @@
-function Drawer({ onClose, items = [] }) {
+function Drawer({ onClose, onRemove, items = [] }) {
   return (
     <div className="overlay">
       <div className="drawer">
@@ -11,40 +11,51 @@ function Drawer({ onClose, items = [] }) {
             onClick={onClose}
           />
         </h2>
-        <div className="items flex">
-          {items.map((obj) => (
-            <div className="cart_item d-flex align-center mb-20">
-              <div
-                style={{ backgroundImage: `url(${obj.image})` }}
-                className="cartItemImg"
-              ></div>
-              <div className="mr-20 flex">
-                <p className="mb-5">{obj.title}</p>
-                <b>${obj.price} руб.</b>
-              </div>
-              <img
-                className="removeBtn"
-                src="/img/btn_cross.svg"
-                alt="remove"
-              />
+        {items.length > 0 ? (
+          <div>
+            <div className="items flex">
+              {items.map((obj) => (
+                <div key={obj.id} className="cart_item d-flex align-center mb-20">
+                  <div
+                    style={{ backgroundImage: `url(${obj.image})` }}
+                    className="cartItemImg"
+                  ></div>
+                  <div className="mr-20 flex">
+                    <p className="mb-5">{obj.title}</p>
+                    <b>${obj.price} руб.</b>
+                  </div>
+                  <img
+                    onClick={() => onRemove(obj.id)}
+                    className="removeBtn"
+                    src="/img/btn_cross.svg"
+                    alt="remove"
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <div className="cartTotalBlock">
-          <ul>
-            <li>
-              <span>Итого: </span>
-              <div></div>
-              <b>21 498 руб. </b>
-            </li>
-            <li>
-              <span>Налог 5%: </span>
-              <div></div>
-              <b>1074 руб. </b>
-            </li>
-          </ul>
-          <button>Оформить заказ</button>
-        </div>
+            <div className="cartTotalBlock">
+              <ul>
+                <li>
+                  <span>Итого: </span>
+                  <div></div>
+                  <b>21 498 руб. </b>
+                </li>
+                <li>
+                  <span>Налог 5%: </span>
+                  <div></div>
+                  <b>1074 руб. </b>
+                </li>
+              </ul>
+              <button>Оформить заказ</button>
+            </div>
+          </div>
+        ) : (
+          <div className="cardTotalBlock d-flex align-center justify-center flex-column flex">
+            <h2>Ваша корзина пуста!</h2>
+            <p>да как так епт</p>
+            <button>вернуться нах</button>
+          </div>
+        )}
       </div>
     </div>
   );
